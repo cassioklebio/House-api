6b36f3215a19d3d923a6171bb5de183c7e577a37
cassio klebio <cassiotr2009@gmail.com>
2024-09-10 15:03:49 -0300

    criando api rest com node js para Aplicacao web de locacao de casa

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import routes from'./routes';
+
+class App{
+    constructor(){
+        this.server = express();
+        this.middleware();
+        this.routes();
+
+    }
+
+    middleware(){
+        this.server.use(express.json());
+        this.server.use(express.urlencoded({ extended: true }));
+
+    }
+
+    routes(){
+        this.server.use(routes);
+
+
+    }
+
+}
+
+export default new App().server;
