2e6daeb6494715d0cc5bdf62e817638b08712c13
cassio klebio <cassiotr2009@gmail.com>
2024-09-10 17:00:32 -0300

    configuracao do mongo DB Atlas

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import routes from'./routes';
 
+
 class App{
     constructor(){
         this.server = express();
+
         this.middleware();
         this.routes();
 
--- src/server.js
+++ src/server.js
@@ -1,3 +1,20 @@
-import app from './app';
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+const app = express()
 
-app.listen(3333);
+
+//aqui vai a URI de sua conexão(que você pega no mongo atlas)
+const uri = 'mongodb+srv://cassiotr2009:t2r2z31n1@devhouse.abzdy.mongodb.net/?retryWrites=true&w=majority&appName=devHouse'
+
+
+//iniciando servidor
+app.listen(3333, () => {
+    console.log('Rodando na porta 3333')
+    //colocamos aqui dentro do listen, para poder fazer a conexão com banco de dados assim que iniciarmos o nosso servidor
+    MongoClient.connect(uri, {
+        
+    }, (err, response) => {
+        if (err) throw err
+        console.log('Conectado') //retorna um 'conectado' no console caso a conexão dê certo
+    })
+})
